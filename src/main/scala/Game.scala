22f4f01d7b356e6f82c38cdbb73378c94496eb5a
Natasha Thrale <nthrale@protonmail.com>
2021-08-03 10:53:58 +0100

    simplify state by calculating remaining lives on the fly

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -3,40 +3,43 @@ package novembertang.hangman
 import scala.annotation.tailrec
 import scala.collection.SortedSet
 
-import Ascii.images
-
 object Game{
 
   @tailrec
   def hangman(state: State): Unit = {
 
     val wordProgress: String = currentWordProgress(state.word, state.guessedLetters)
-
-    printCurrentProgress(state.guessedLetters, state.lives, wordProgress)
-
+    val livesLeft: Int = calculateRemainingLives(state)
+    val outOfLives = livesLeft == 0
     val finishedWord = wordProgress == state.word
-    val livesRemaining = state.lives > 0
 
-    (livesRemaining, finishedWord) match {
-      case (false, _) => println(s"Out of lives. You lose!\nThe word was ${state.word}")
-      case (true, true) => println("You win!")
-      case (true, false) =>
+    printCurrentProgress(state.guessedLetters, livesLeft, wordProgress)
+
+    (outOfLives, finishedWord) match {
+      case (true, _) => println(s"Out of lives. You lose!\nThe word was ${state.word}")
+      case (false, true) => println("You win!")
+      case (false, false) =>
         val letter: Char = IO.getLetter(state.guessedLetters)
         val updatedLetterSet = state.guessedLetters + letter
         if (state.word.contains(letter)) {
           println(s"$letter was correct!")
-          hangman(State(state.word, updatedLetterSet, state.lives))
+          hangman(State(state.word, updatedLetterSet))
         }
         else {
           println(s"$letter was incorrect")
-          hangman(State(state.word, updatedLetterSet, state.lives - 1))
+          hangman(State(state.word, updatedLetterSet))
         }
     }
   }
 
+  private def calculateRemainingLives(state: State) = {
+    val incorrectGuesses: SortedSet[Char] = state.guessedLetters -- state.word.toSet
+    Ascii.totalLives - incorrectGuesses.size
+  }
+
   private def printCurrentProgress(guessedLetters: SortedSet[Char], lives: Int, wordProgress: String): Unit = {
     println(s"\n\nCurrent Progress: $wordProgress\n$lives ${if (lives==1) "life" else "lives"} remaining")
-    println(images(lives))
+    println(Ascii.images(lives))
 
     if (guessedLetters.nonEmpty) {
       print(s"Guessed Letters: ")
--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -2,8 +2,8 @@ package novembertang.hangman
 
 import scala.collection.SortedSet
 
-case class State(word: String, guessedLetters: SortedSet[Char], lives: Int)
+case class State(word: String, guessedLetters: SortedSet[Char])
 
 object State {
-  def apply(word: String): State = State(word, SortedSet.empty[Char], Ascii.totalLives)
+  def apply(word: String): State = State(word, SortedSet.empty[Char])
 }
